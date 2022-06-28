a65b36fae8fbd2981a6218547f19c38178374d11
D4reD3VIL81 <ahmad.81.sadeghi@gmail.com>
2022-06-28 10:43:11 +0430

    Serach filter in REST framework

--- profile_project/profiles_api/views.py
+++ profile_project/profiles_api/views.py
@@ -7,6 +7,9 @@ from yaml import serialize_all
 from . import serializers
 from rest_framework import viewsets
 from . import models
+from rest_framework.authentication import TokenAuthentication
+from . import permissions
+from rest_framework import filters
 
 # Create your views here.
 
@@ -107,4 +110,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
-    
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
