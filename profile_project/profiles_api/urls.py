d1f116b06012e32090955fefad31e0206ea96fde
D4reD3VIL81 <ahmad.81.sadeghi@gmail.com>
2022-06-28 09:39:02 +0430

    asd

--- profile_project/profiles_api/urls.py
+++ profile_project/profiles_api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from django.urls import include
+from . import views
 
 app_name = ''
 
-url_patterns = [
-    path
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
